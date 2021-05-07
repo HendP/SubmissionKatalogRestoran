28c1614e95b7255c79606106de1262ee7cc77eb0
HendP <hendpermana12@gmail.com>
2021-05-07 10:05:40 +0700

    add catalog and footer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,8 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import '../styles/responsive.css';
-import '../scripts/hamburgerScript.js';
+import './toggle.js';
+import './catalog.js'
 
 
 console.log('Hello Coders! :)');
